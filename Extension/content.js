f9de405532d8e1ebddc3d3e666e087e4a39e0e33
Aden <adenman@hotmail.co.nz>
2022-07-23 22:37:24 +1200

    example communication between background and content

---
+++ Extension/content.js
@@ -0,0 +1,5 @@
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+      console.log(response.farewell);
+    });
+  });
