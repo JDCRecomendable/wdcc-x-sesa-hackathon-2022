0949e502c21df08b8dd155ac4dfdf7bbc10354db
Dhruv Joshi <djos192@aucklanduni.ac.nz>
2022-07-23 16:52:29 +1200

    Added grid to shoppage

--- frontend/src/pages/ShopPage.js
+++ frontend/src/pages/ShopPage.js
@@ -1,8 +1,50 @@
+import Grid from '@mui/material/Grid'
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+function FormRow() {
+  return (
+    <React.Fragment>
+      <Grid item xs={4}>
+        <Item>Item</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>Item</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>Item</Item>
+      </Grid>
+    </React.Fragment>
+  );
+}
+
 const ShopPage = () => {
   return (
-    <>
-      <h1>Shop</h1>
-    </>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1}>
+          <Grid container item spacing={3}>
+            <FormRow />
+          </Grid>
+          <Grid container item spacing={3}>
+            <FormRow />
+          </Grid>
+          <Grid container item spacing={3}>
+            <FormRow />
+          </Grid>
+        </Grid>
+      </Box>
+  
   );
 };
 
