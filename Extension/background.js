24a0acbdfe1a65f65cd1d71ed9a28da0a1a88b48
Aden <adenman@hotmail.co.nz>
2022-07-23 16:29:43 +1200

    progress bar reset when full

--- Extension/background.js
+++ Extension/background.js
@@ -1,4 +1,4 @@
-let time = 25;
+let time = 50;
 chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
     if(message.method == "getTime"){
       sendResponse(time);
--- Extension/popup.js
+++ Extension/popup.js
@@ -4,13 +4,16 @@ chrome.runtime.sendMessage({method:"getTime"},function(response){
     let time = response;
     const progressEl = document.getElementById("progressBar");
     progressEl.value = time;
+    
     let coinTimer = setInterval(() => {
       progressEl.value++;
 
       if (progressEl.value >= progressEl.max) {
-        clearInterval(coinTimer);
+        progressEl.value = 0;
         console.log("Would replace");
+        
      }
-    }, 200);
+    }, 1000);
 });
+
 
