043ceb444dbf018e61c7074b4a4c3218a34a1f2f
Dhruv Joshi <djos192@aucklanduni.ac.nz>
2022-07-23 16:54:11 +1200

    Added grid to shoppage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,7 @@ import RoomsPage from "./pages/RoomsPage";
 import ShopPage from "./pages/ShopPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { createTheme } from "@mui/material/styles";
-import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
-
 function App() {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#371B58",
-      },
-      secondary: {
-        main: "#371B58",
-      },
-    },
-  });
-
   return (
     <Router>
       <>
