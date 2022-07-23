0656c8f505f82979ee2a1f2a5d80cbbe8d16fbde
Dhruv Joshi <djos192@aucklanduni.ac.nz>
2022-07-24 01:19:37 +1200

    Updated page style to utilising Divs

--- frontend/src/pages/RoomsPage.js
+++ frontend/src/pages/RoomsPage.js
@@ -1,10 +1,11 @@
-import Sidebar from "../components/SideBar";
-
 const RoomsPage = () => {
   return (
-    <> <Sidebar />
-      <div> Rooms Page </div></>
-  );  
-};  
+    <> <div style={{ position: 'fixed', width: '18%', height: '100%', zIndex: '1', top: '3.4em', backgroundColor: '#E0D4FF', overflowX: 'hidden', marginTop: '18px' }}>
+      <button 
+      sx={{ my: 2, color: "white", display: "block",marginLeft: "20px"}} >Create</button>
+    </div>
+     <div style={{ position: 'fixed', width: '20%', height: '40%', zIndex: '1', top: '3.4em', backgroundColor: '#E0D4FF', overflowX: 'hidden', marginTop: '50px', marginLeft: '30%', borderRadius: '30px'}}>  </div></>
+  );
+};
 
 export default RoomsPage;
