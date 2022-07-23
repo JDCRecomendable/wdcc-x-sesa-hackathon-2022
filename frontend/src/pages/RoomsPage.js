9a4859e55867d9546155c80639f082297bab5ffd
zoe <zoejniu@gmail.com>
2022-07-24 02:10:58 +1200

    Add table and buttons

--- frontend/src/pages/RoomsPage.js
+++ frontend/src/pages/RoomsPage.js
@@ -17,7 +17,6 @@ const RoomsPage = () => {
 
   return (
     <>
-      {" "}
       <div
         style={{
           position: "fixed",
@@ -30,12 +29,33 @@ const RoomsPage = () => {
           marginTop: "18px",
         }}
       >
-        <button
-          sx={{ my: 2, color: "white", display: "block", marginLeft: "20px" }}
-        >
-          Create
-        </button>
+        Table
+        <table>
+          <tr>
+            <th>Room Name</th>
+            <th>Room ID</th>
+          </tr>
+          {roomsList}
+        </table>
+        <div style={{ float: "left" }}>
+          <CreateRoom />
+          <JoinRoom />
+        </div>
       </div>
+      <div
+        style={{
+          position: "fixed",
+          width: "20%",
+          height: "40%",
+          zIndex: "1",
+          top: "3.4em",
+          backgroundColor: "#E0D4FF",
+          overflowX: "hidden",
+          marginTop: "50px",
+          marginLeft: "30%",
+          borderRadius: "30px",
+        }}
+      ></div>
       <div
         style={{
           position: "fixed",
